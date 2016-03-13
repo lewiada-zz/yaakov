fb8f96a48e7b57c60dbf37dee47ca3982b1210db
Adam Lewis <lewiada@iit.edu>
2016-03-13 10:36:54 -0500

    test

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+//
+
 app.listen(process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 //app.use('/assets', express.static(__dirname + '/public'));
