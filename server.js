28a2496449b3bc46b3ed5f7a199205a42dc7b763
Adam Lewis <lewiada@iit.edu>
2016-03-13 10:26:42 -0500

    initial project version

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.listen(process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+//app.use('/assets', express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+
+// begin on the 'home page'
+app.get('/', function(req, res) {
+    res.render('index');
+});
